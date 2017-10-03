a1435a588d9ca233437ce1037166fa0a15a88cf2
lymanjohnson <lyman.johnson@gmail.com>
2017-10-02 22:06:28 -0400

    Adds sessions controller create,new,destroy and supporting methods in application_record

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_session_path
+  end
 end
