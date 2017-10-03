8461370bbdf592486f5688ebe6b571773bb0e6c2
lymanjohnson <lyman.johnson@gmail.com>
2017-10-02 22:30:40 -0400

    Removes certain users resources from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :answers
   resources :questions
   resources :quizzes
-  resources :users
+  resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
