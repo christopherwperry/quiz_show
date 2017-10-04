f49709d87efbffc77915fd913abedfd4cf8eeaf3
lymanjohnson <lyman.johnson@gmail.com>
2017-10-04 12:13:50 -0400

    Fixes additional bug. Now routing for admin, user, and not signed in behavior for /users/ paths works.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    @current_user = User.find(session[:user_id]) if session[:user_id]
     # current_user ||= User.find(session[:user_id]) if session[:user_id]
     # current_user = User.find(session[:user_id])
     # User.find(session[:user_id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,22 @@
 class UsersController < ApplicationController
-  before_action :current_user, only: [:index, :destroy, :publish]
+  before_action :current_user #, only: [:index, :destroy, :publish]
 
   def new
     @user = User.new
   end
 
   def index
-    if current_user
-      @user = current_user
-      if current_user.is_admin?
+    if @current_user
+      if @current_user.is_admin?
         @users = User.all
         render 'index'
       else
-        redirect_to user_path(@user)
+        redirect_to user_path(@current_user)
       end
     else
       redirect_to root_path
     end
   end
-
-
-
-    # elsif current_user
-    #   @user = current_user
-    #   redirect_to user_path(@user)
-    # else
-    #   redirect_to root_path
-    # end
 
   def create
     @user = User.new(user_params)
@@ -39,16 +29,20 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    if @user == current_user or current_user.is_admin?
-      render 'show'
+    if @current_user
+      if @user == @current_user or @current_user.is_admin?
+        render 'show'
+      else
+        redirect_to users_path
+      end
     else
-      redirect_to users_path
+      redirect_to root_path
     end
   end
 
   def destroy
     @user = User.find(params[:id])
-    if @user == current_user
+    if @user == @current_user
       @user.destroy
       redirect_to '/'
     else
@@ -62,6 +56,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :password)
+  end
+
+  def is_current_user
+    redirect_to root_path if @current_user.nil?
   end
 
 end
